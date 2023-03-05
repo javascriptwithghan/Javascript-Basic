c51f67d05eff0a461b6ea0f81606e8056e07d57c
codewithghan <info@codewithghan.com>
2023-03-05 11:19:24 +0530

    push, pop, shift, unshift in array basic

--- array_basic_js.js
+++ array_basic_js.js
@@ -1,3 +1,22 @@
 const numberArray = [10,20,30];
 
+console.log(numberArray);  // Printing array
+
+// Array push
+numberArray.push(100);
+console.log(numberArray); // Added 100 in last index
+
+// Array pop
+
+numberArray.pop();
+
+console.log(numberArray);  // Remove 100 from last index
+
+// shifting array from start index
+numberArray.shift();
 console.log(numberArray);
+
+// Adding element at start of index
+numberArray.unshift('John');
+console.log(numberArray); // O/P : ['John', 20, 30]
+
